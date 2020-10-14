ede47a2e9b60fcebfe519272ef5f7fee54cf649d
Willow Behar <willow.behar@gmail.com>
2020-10-14 16:24:36 -0500

    Changed generate for asteroids
    Changed generate to take in two Vector2s instead of 4 variables

--- Assets/Scripts/GenerateAsteroids.gd
+++ Assets/Scripts/GenerateAsteroids.gd
@@ -6,14 +6,10 @@ export var min_scale = 0.5
 export var max_scale = 2
 export (PackedScene) var Obj
 
-
-func _ready():
-	pass # Replace with function body.
-
-func generate(x,y,width, height):
+func generate(pos, dim):
 	for i in range(amount):
-		var nx = rand_range(x-(width/2), x+(width/2))
-		var ny = rand_range(y-(height/2), y+(height/2))
+		var nx = rand_range(pos.x-(dim.x/2), pos.x+(dim.x/2))
+		var ny = rand_range(pos.y-(dim.y/2), pos.y+(dim.y/2))
 		var rot = rand_range(0, 360)
 		var size = rand_range(min_scale, max_scale)
 		var a = Obj.instance()
