e2784acb2dfc4ce8157f05bb9a54bb46ff92b6db
Willow Behar <willow.behar@gmail.com>
2020-10-14 14:37:36 -0500

    Updated Planets Area2D Functions
    Added Planets destroying Asteroids that come too close
    Added Player can_scan variable update when entering and leaving

--- Assets/Scripts/Planet.gd
+++ Assets/Scripts/Planet.gd
@@ -18,6 +18,17 @@ func _physics_process(delta):
 
 
 func _on_ScanArea_body_entered(body):
-	#if body.has_variable("can_scan") and not been_scanned:
-	#	body.can_scan = true
+	if body.is_in_group("Asteroid"):
+		body.take_damage(10000)
+	if body.is_in_group("Player"):
+		if not been_scanned:
+			body.can_scan = true
+			print("Can Scan")
+	pass # Replace with function body.
+
+
+func _on_ScanArea_body_exited(body):
+	if body.is_in_group("Player"):
+		body.can_scan = false
+		print("Can't Scan")
 	pass # Replace with function body.
