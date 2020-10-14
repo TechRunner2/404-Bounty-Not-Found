fed456b47a6d4070c601dd18be02b9591fa296e8
Willow Behar <willow.behar@gmail.com>
2020-10-14 14:11:49 -0500

    Added Variables and Movement
    Added Max Speed, Damage done to objects it collides with, damage, and a direction vector. It randomly gets assigned a value between -1 and 1 and multiplies it my the max speed

--- Assets/Scripts/Asteroid.gd
+++ Assets/Scripts/Asteroid.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 export var health = 5
+export var max_speed = 2
+export var damage = 2
+var direction = Vector2()
 
 func take_damage(amount):
 	health -= amount
@@ -8,7 +11,16 @@ func take_damage(amount):
 		queue_free()
 
 func _ready():
+	direction.x = rand_range(-1, 1) * max_speed
+	direction.y = rand_range(-1, 1) * max_speed
 	pass # Replace with function body.
 
+func _process(delta):
+	pass
+
 func _physics_process(delta):
+	var collision = move_and_collide(direction)
+	if collision:
+		if collision.collider.has_method("take_damage"):
+			collision.collider.take_damage(damage)
 	pass
