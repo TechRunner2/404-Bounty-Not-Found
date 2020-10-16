61ae166ceeb62b374fcccc0f3e0f7fd2db05d7da
Willow Behar <willow.behar@gmail.com>
2020-10-16 18:21:34 -0500

    Attempted To rewrite the Generation and failed
    Not really sure what I'm doing wrong

--- Assets/Scripts/Game.gd
+++ Assets/Scripts/Game.gd
@@ -7,15 +7,15 @@ var last_star_gen = Vector2()
 var menu_open = false
 
 #Quad locations
-var top_left = Vector2(-quad_dimension.x, -quad_dimension.y) * 2
-var top_center = Vector2(0, -quad_dimension.y) * 2
-var top_right = Vector2(quad_dimension.x, -quad_dimension.y) *2
-var center_left = Vector2(-quad_dimension.x, 0) *2 
+var top_left = Vector2(-quad_dimension.x, -quad_dimension.y)
+var top_center = Vector2(0, -quad_dimension.y)
+var top_right = Vector2(quad_dimension.x, -quad_dimension.y)
+var center_left = Vector2(-quad_dimension.x, 0)
 var center = Vector2(0,0)
-var center_right = Vector2(0, quad_dimension.y) *2 
-var bottom_left = Vector2(-quad_dimension.x, quad_dimension.y) * 2
-var bottom_center = Vector2(0, quad_dimension.y) * 2 
-var bottom_right = Vector2(quad_dimension.x, quad_dimension.y) * 2
+var center_right = Vector2(0, quad_dimension.y)
+var bottom_left = Vector2(-quad_dimension.x, quad_dimension.y)
+var bottom_center = Vector2(0, quad_dimension.y) 
+var bottom_right = Vector2(quad_dimension.x, quad_dimension.y)
 
 func _ready():
 	current_pos = Vector2(0,0)
@@ -34,56 +34,75 @@ func _on_Player_death(dead):
 	pass 
 
 func generate_up():
-	var left = Vector2(current_pos.x - quad_dimension.x, current_pos.y - quad_dimension.y)
-	var center = Vector2(current_pos.x, current_pos.y - quad_dimension.y)
-	var right = Vector2(current_pos.x + quad_dimension.x, current_pos.y - quad_dimension.y)
-	generate_quadrant(left)
-	generate_quadrant(center)
-	generate_quadrant(right)
-	current_pos = center
+	current_pos += top_center
+	var left = current_pos + top_left
+	var center = current_pos + top_center
+	var right = current_pos + top_right
+	if not left in prev_locations:
+		generate_quadrant(left)
+		prev_locations.append(left)
+	if not center in prev_locations:
+		generate_quadrant(center)
+		prev_locations.append(center)
+	if not right in prev_locations:
+		generate_quadrant(right)
+		prev_locations.append(right)
 	pass
 
 func generate_down():
-	var left = Vector2(current_pos.x - quad_dimension.x, current_pos.y + quad_dimension.y)
-	var center = Vector2(current_pos.x, current_pos.y + quad_dimension.y)
-	var right = Vector2(current_pos.x + quad_dimension.x, current_pos.y + quad_dimension.y)
-	generate_quadrant(left)
-	generate_quadrant(center)
-	generate_quadrant(right)
-	current_pos = center
+	current_pos += bottom_center
+	var left = current_pos + bottom_left
+	var center = current_pos + bottom_center
+	var right = current_pos + bottom_right
+	if not left in prev_locations:
+		generate_quadrant(left)
+		prev_locations.append(left)
+	if not center in prev_locations:
+		generate_quadrant(center)
+		prev_locations.append(center)
+	if not right in prev_locations:
+		generate_quadrant(right)
+		prev_locations.append(right)
 	pass
 
 func generate_left():
-	var new_x = current_pos.x - (quad_dimension.x * 2)
-	var top = Vector2(current_pos.x - quad_dimension.x, current_pos.y - quad_dimension.y)
-	var left = Vector2(current_pos.x - quad_dimension.x, current_pos.y)
-	var bottom = Vector2(current_pos.x - quad_dimension.x, current_pos.y + quad_dimension.y)
-	generate_quadrant(top)
-	generate_quadrant(left)
-	generate_quadrant(bottom)
-	current_pos = left
+	current_pos += center_left
+	var top = current_pos + top_left
+	var left = current_pos + center_left
+	var bottom = current_pos + bottom_left
+	if not top in prev_locations:
+		generate_quadrant(top)
+		prev_locations.append(top)
+	if not left in prev_locations:
+		generate_quadrant(left)
+		prev_locations.append(top)
+	if not bottom in prev_locations:
+		generate_quadrant(bottom)
+		prev_locations.append(bottom)
 	pass
 
 func generate_right():
-	var new_x = current_pos.x + (quad_dimension.x * 2)
-	var top = Vector2(new_x, current_pos.y - quad_dimension.y)
-	var right = Vector2(new_x, current_pos.y)
-	var bottom = Vector2(new_x, current_pos.y + quad_dimension.y)
-	generate_quadrant(top)
-	generate_quadrant(right)
-	generate_quadrant(bottom)
-	current_pos = right
+	current_pos += center_right
+	var top = current_pos + top_right
+	var right = current_pos + center_right
+	var bottom = current_pos + bottom_right
+	if not top in prev_locations:
+		generate_quadrant(top)
+		prev_locations.append(top)
+	if not right in prev_locations:
+		generate_quadrant(right)
+		prev_locations.append(right)
+	if not bottom in prev_locations:
+		generate_quadrant(bottom)
+		prev_locations.append(bottom)
 	pass
 
 func generate_quadrant(pos):
-	if not pos in prev_locations:
-		#Generate Stars
-		$Stars.generate(pos, quad_dimension)
-		#Generate Asteroids
-		$Asteroids.generate(pos, quad_dimension)
-		#Generate Planets
-		#Add position to previous generation locations
-		prev_locations.append(pos)
+	#Generate Stars
+	$Stars.generate(pos, quad_dimension)
+	#Generate Asteroids
+	$Asteroids.generate(pos, quad_dimension)
+	#Generate Planets
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -92,17 +111,20 @@ func _process(delta):
 
 	var pos = $Player.position
 	#Player Exits Current Quadrant to right
-	#if pos.x >= current_pos.x + quad_dimension.x:
-		#generate_right()
-	#Player Exits quad to left
-	#if pos.x <= current_pos.x - quad_dimension.x:
-		#generate_left()
+	if pos.x >= current_pos.x + (quad_dimension.x/2):
+		print("Gen Right")
+		generate_right()
+	else: 
+		if pos.x <= current_pos.x - (quad_dimension.x/2):
+			print("Gen Left")
+			generate_left()
+	if pos.y >= current_pos.y + (quad_dimension.y/2):
+		print("Gen Down")
+		generate_down()
+	else:
+		if pos.y <= current_pos.y - (quad_dimension.y/2):
+			print("Gen Up")
+			generate_up()
 		
-	#Player Exits quad to up
-	#if pos.y >= current_pos.y - quad_dimension.y:
-		#generate_up()
-	#Player Exits to bottom
-	#if pos.y <= current_pos.y + quad_dimension.y:
-		#generate_down()
 	pass
 
