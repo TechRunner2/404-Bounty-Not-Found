7c592a3a7ad8ce6574ecdd2f83ced87dbe08255b
Willow Behar <willow.behar@gmail.com>
2020-10-16 18:46:53 -0500

    Removed un-needed Variable

--- Assets/Scripts/Stars.gd
+++ Assets/Scripts/Stars.gd
@@ -10,9 +10,8 @@ func generate(pos, dim):
 		var nx = rand_range(-pos.x-(dim.x/2), pos.x+(dim.x/2))
 		var ny = rand_range(-pos.y-(dim.y/2), pos.y+(dim.y/2))
 		var rot = rand_range(0, 360)
-		var location = Vector2(nx, ny)
 		var star = stars[int(rand_range(0, stars.size()))]
 		var s = star.instance()
-		s.position = location
+		s.position = Vector2(nx, ny)
 		s.rotation = rot
 		add_child(s)
