38e7945c35dbeebd44ef9530c5cc26bc1544da24
Willow Behar <bbrandon.behar@protonmail.com>
2020-10-16 22:29:45 -0500

    Fixed label anchor issues

--- Scenes/End Screen.gd
+++ Scenes/End Screen.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _input(event):
 	if event.is_action("primary_fire"):
-		get_tree().change_scene_to("res://Scenes/Game.tscn")
+		get_tree().change_scene("res://Scenes/Game.tscn")
 
 
 func _on_End_Screen_tree_entered():
