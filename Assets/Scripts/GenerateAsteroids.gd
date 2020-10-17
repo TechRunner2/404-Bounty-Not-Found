e98098ea816422d94dd6392f78f6e6a2e85856ad
Willow Behar <willow.behar@gmail.com>
2020-10-17 10:26:20 -0500

    Made Variables more understandable

--- Assets/Scripts/GenerateAsteroids.gd
+++ Assets/Scripts/GenerateAsteroids.gd
@@ -4,18 +4,18 @@ extends Node
 export var amount = 5
 export var min_scale = 0.5
 export var max_scale = 2
-export (PackedScene) var Obj
+export (PackedScene) var asteroid_scene
 
-func generate(pos, dim):
-	for i in range(amount):
+func generate(pos, dim, quantity):
+	for i in range(quantity):
 		var nx = rand_range(-pos.x-(dim.x/2), pos.x+(dim.x/2))
 		var ny = rand_range(-pos.y-(dim.y/2), pos.y+(dim.y/2))
 		var rot = rand_range(0, 360)
 		var size = rand_range(min_scale, max_scale)
-		var a = Obj.instance()
-		a.position = Vector2(nx, ny)
-		a.rotation = rot
-		a.scale = Vector2(size, size)
-		add_child(a)
+		var asteroid = asteroid_scene.instance()
+		asteroid.global_position = Vector2(nx, ny)
+		asteroid.rotation = rot
+		asteroid.scale = Vector2(size, size)
+		add_child(asteroid)
 		
 		
