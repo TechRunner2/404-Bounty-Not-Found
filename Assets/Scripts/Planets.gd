cb78269586ff7ab2fe869418e36619c7dc17e61d
Willow Behar <willow.behar@gmail.com>
2020-10-17 10:41:54 -0500

    Created Planet placement script

--- Assets/Scripts/Planets.gd
+++ Assets/Scripts/Planets.gd
@@ -1,6 +1,12 @@
 extends Node
 
+export (PackedScene) var planet_scene
 
-
-func generate_planets(x, y, width, height):
+func generate(pos, dim, quantity):
+	for i in range(quantity):
+		var new_pos = Vector2(rand_range(pos.x - (dim.x/2), pos.x + (dim.x/2)),
+							  rand_range(pos.y - (dim.y/2), pos.y + (dim.y/2)))
+		var planet = planet_scene.instance()
+		planet.position = new_pos
+		add_child(planet)
 	pass
