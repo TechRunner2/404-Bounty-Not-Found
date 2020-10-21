9d6fbca6b6623ced318685cf45301a502ab53b71
Willow Behar <willow.behar@gmail.com>
2020-10-21 16:27:58 -0500

    Made Lasers it's own thing
    Added Sound and timer

--- Assets/Scripts/Laser.gd
+++ Assets/Scripts/Laser.gd
@@ -2,9 +2,19 @@ extends Position2D
 
 
 export (PackedScene) var Projectile
+var can_shoot = true
 
 func shoot(rot):
-	var b = Projectile.instance()
-	b.start(global_position, rot)
-	get_tree().get_root().add_child(b)
+	if can_shoot:
+		var b = Projectile.instance()
+		b.start(global_position, rot)
+		get_tree().get_root().add_child(b)
+		$FireSound.play()
+		can_shoot = false
+		$Timer.start()
 	pass
+
+
+func _on_Timer_timeout():
+	can_shoot = true
+	pass # Replace with function body.
