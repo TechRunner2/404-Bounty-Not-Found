aeba7c9416fec4b5aeb57f4675940f759106f9ad
Willow Behar <willow.behar@gmail.com>
2020-10-22 11:46:29 -0500

    Removed Unused code from Main Menu

--- Assets/Scripts/Main Menu.gd
+++ Assets/Scripts/Main Menu.gd
@@ -1,28 +1,8 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var window = get_viewport_rect()
-	#$Stars.generate_stars(window.position.x + window.size.x/2, window.position.y + window.size.y / 2, window.size.x, window.size.y)
-	#$Asteroids.generate(window.position.x + window.size.x/2, window.position.y + window.size.y / 2, window.size.x, window.size.y)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Exit_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
-
 
 func _on_About_pressed():
 	get_tree().change_scene("res://Scenes/About.tscn")
